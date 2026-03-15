7b2a230fed7080596dbf83d75d378d914b2a56e4
pushkalmehrotra <pushkalmehrotra774@gmail.com>
2026-03-15 15:58:41 +0530

    initial.commit

---
+++ se.py
@@ -0,0 +1,15 @@
+def SievefEratosthenes (num):
+    prime = [True for i in range(num+1)]
+    p=2
+    while (P * p<= num):
+        if (prime[p] == True):
+            for i in range(p * p, num+1, p):
+                prime[i] = False
+        p+=1
+    for p in range(2, num+1):
+        if prime[p]:
+            print (p)
+num = int(input("Enter a number"))
+print("Following are the prime numbers smaller")
+print("than or equal to", num)
+SievefEratosthenes (num)
